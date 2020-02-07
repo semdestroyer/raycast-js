c4a499c6fb5cf218cbeb943c7a7810e854c48edd
sem <semdestroyer@ya.ru>
2020-02-07 20:30:29 +0300

    first commit

---
+++ scripts/Camera.js
@@ -0,0 +1,25 @@
+var range = 200;
+var rays = 512;
+var height = 512;
+
+
+
+
+
+
+function draw()
+{
+    for(var i = 0; i<rays;i++)
+    {
+        for(var c; c<range; c+=0.2)
+        {
+            if(c == ///maploader.map.cellsize[/cellsize][/cellsize] !=0 )
+            {
+                break;
+            }
+        }
+
+
+    }
+
+}
---
+++ scripts/Index.js
@@ -0,0 +1,12 @@
+
+
+
+function start()
+{
+    
+}
+
+function update()
+{
+
+}
